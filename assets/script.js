866d32313f7c9f8659ac99c8bec0fed433d53754
Bruno Sobral - siscon <brunosobralss@hotmail.com>
2022-12-23 13:55:19 -0300

    finished typing

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+/* typing */
+var typed = new Typed('.typing',{
+  strings: ['Full Stack Developer','Web Designer', 'Web Developer', 'Graphic Designer'],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+})
